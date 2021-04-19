890d852ffc42213bdcc1c79bcdfeb23bdafc0a9d
polbac <polbac@gmail.com>
2021-04-19 18:22:40 -0300

    aa

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>TRIMARCHI</title>
+        <link rel="stylesheet" href="assets/style.css">
     </head>
     <body>
     <?php require('modules/form.php'); ?>
--- modules/form.php
+++ modules/form.php
@@ -7,36 +7,104 @@ $countriesAndCities = file_get_contents('countries.min.json');
     const countries = JSON.parse('<?php echo $countriesAndCities; ?>');
 </script>
 
-<form name="register-form">
+<form name="register-form" class="">
     <div class="form-step">
-        <div class="form-step-title">Paso 2 Completa el formulario</div>
-        <div class="field-wrapper">
-            <input name="user" id="user" type="text" placeholder="Tu usuario / Perfil Domestika" required />
-        </div>
-
-        <div class="field-wrapper">
-            <input name="name" id="name" type="text" placeholder="Tu Nombre / Apellido" required />
-        </div>
-
-        <div class="field-wrapper">
-            <select name="country" id="country" type="text" required></select>
-        </div>
-
-        <div class="field-wrapper">
-            <select name="city" id="city" required></select>
-        </div>
-
-        <div class="action-wrapper">
-            <button type="submit">ENVIAR</buton>
+        <div class="form-step-title">
+            <p>
+                Paso 1 Registrate en <a href="https://www.domestika.org/es" target="_blank">Doméstika</a>
+            </p>
+            <p>
+                Completa tu perfil con foto
+            </p>
         </div>
     </div>
+    <div class="form-step">
+        <div class="form-step-title">Paso 2 Completa el formulario</div>
+        
+        <div class="field-wrapper">
+            <input name="Usuario" id="usuario" type="text" placeholder="Tu usuario / Perfil Domestika" required />
+        </div>
+
+        <div class="field-wrapper">
+            <input name="Nombre" id="nombre" type="text" placeholder="Tu Nombre / Apellido" required />
+        </div>
+
+        <div class="field-wrapper">
+            <input name="Edad" id="edad" type="text" placeholder="Edad" required />
+        </div>
+
+        <div class="field-wrapper">
+            <select name="Pais" id="pais" type="text" required></select>
+        </div>
+
+        <div class="field-wrapper">
+            <select name="Ciudad" id="ciudad" required></select>
+        </div>
+
+        
+    </div>
+
+
+    <div class="form-step">
+        <div class="form-step-title">Paso 3 Elegi la/s categoría/s</div>
+        
+        <div class="field-wrapper">
+            
+            <div class="field-option">
+                <input type="checkbox" name="category" id="Conferencia" value="Conferencia">
+                <label for="conferencia">Conferencia</label>
+            </div>
+
+            <div class="field-option">
+                <input type="checkbox" name="category" id="Obra" value="Obra">
+                <label for="Obra">Obra</label>
+            </div>
+
+            <div class="field-option">
+                <input type="checkbox" name="category" value="Workshop" id="Workshop">
+                <label for="Workshop">Workshop</label>
+            </div>
+
+            <div class="field-option">
+                <input type="checkbox" name="category" id="Proyecto" value="Proyecto">
+                <label for="Proyecto">Proyecto</label>
+            </div>
+
+            <div class="field-option">
+                <input type="checkbox" name="category" id="Música" value="Música">
+                <label for="Música">Música</label>
+            </div>
+
+            <div class="field-option">
+                <input type="checkbox" name="category" id="Videoclip" value="Videoclip">
+                <label for="Videoclip">Videoclip</label>
+            </div>
+
+            <div class="field-option">
+                <input type="checkbox" name="category" id="Portada" value="Portada">
+                <label for="Portada">Portada</label>
+            </div>
+
+            <div class="field-option">
+                <input type="checkbox" name="category" id="Colectivo TRImarchi" value="Colectivo TRImarchi">
+                <label for="Colectivo TRImarchi">Colectivo TRImarchi</label>
+            </div>
+            
+        </div>
+
+    </div>
+
+    <div class="action-wrapper">
+            <button type="submit">ENVIAR</buton>
+        </div>
 
 </form>
 
 <script>
-    const country = document.querySelector('#country')
-    const city = document.querySelector('#city')
-    
+    const country = document.querySelector('#pais')
+    const city = document.querySelector('#ciudad')
+    const categories = document.querySelectorAll('input[name="category"]')
+
     country.innerHTML = Object.keys(countries)
         .map(c => `<option value='${c}' ${c === 'Argentina' ? 'selected' : ''}>${c}</option>`)
     
@@ -51,13 +119,25 @@ $countriesAndCities = file_get_contents('countries.min.json');
 
     showCities('Argentina')
 
-    const form = document.forms['register-form']
+    
 
+
+    const form = document.forms['register-form']
+    
     form.addEventListener('submit', e => {
         e.preventDefault()
-        fetch(API_SAVE, { method: 'POST', body: new FormData(form)})
+        const formData = new FormData(form)
+
+        categories.forEach(category => {
+            if (category.checked) {
+                formData.append(category.getAttribute('value'), 'x')
+            }
+        })
+        
+        fetch(API_SAVE, { method: 'POST', body: formData })
             .then((res) => {
                 console.log(res.status);
+                form.reset()
                 return res.text();
             })
             .then((res) => console.log(res))
