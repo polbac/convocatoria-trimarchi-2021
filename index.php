6572e821136aa29ca8eb8a2977c7cd29ab4043ad
polbac <polbac@gmail.com>
2021-05-27 17:37:06 -0300

    falopas

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <html>
     <head>
         <title>TRIMARCHI</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="assets/style.css">
+        <link rel="stylesheet" href="assets/form.css">
     </head>
     <body>
 
--- modules/form.php
+++ modules/form.php
@@ -7,52 +7,107 @@ $countriesAndCities = file_get_contents('countries.min.json');
     const countries = JSON.parse('<?php echo $countriesAndCities; ?>');
 </script>
 
-<form name="register-form" class="">
+<form name="register-form" class="egister-form">
+    <h2 class="big-title">INSCRIPCIÓN</h2>
+    
+
+    <p class="big-title-2">
+        PASO 1
+    </p>
+    <p class="big-title-2 red">
+        Registrate en Domestika
+    </p>
+    <p class="red center">
+        Podés hacerlo acá. Al crear tu perfil, te recomendamos hacerlo de manera completa (agregá una foto de perfil y contanos algo sobre vos). Eso nos ayudará a conocerte más y te dará mayores chances de ser elegido. <br/>
+        Si tenés dudas, consulta las FAQs o encontranos en convocatoriatrimarchi2021@gmail.com<br/>
+        Si ya estás registrado en Domestika avanzá al Paso 2.
+    </p>
+
+
     <div class="form-step">
-        <div class="form-step-title">
+        
+        <p class="big-title-2">
+            PASO 2
+        </p>
+        <p class="big-title-2">
+            Completá el formulario
+        </p>
+        
+        <div class="field-wrapper">
+            <div class="flex-column">
+                <label>Usuario en Domestika*</label>
+                <input placeholder="@USERNAME" class="grow" name="Usuario" id="usuario" type="text"  required />
+            </div>
             <p>
-                Paso 1 Registrate en <a href="https://www.domestika.org/es" target="_blank">Doméstika</a>
-            </p>
-            <p>
-                Completa tu perfil con foto
+                Si ya estás registrado en Domestika pero no sabés cómo consultar<br/>
+                tu nombre de usuario, chequeá las FAQs.
             </p>
         </div>
+
+        <div class="field-wrapper flex-column">
+            <label>Nombre y Apellido*</label>
+            <input class="grow" name="Nombre" id="nombre" type="text" required />
+        </div>
+
+        <div class="field-wrapper flex-column">
+            <label>Edad*</label>
+            <input class="grow" name="Edad" id="edad" type="text" required />
+        </div>
+
+        <div class="field-wrapper flex-column">
+            <label>E-mail*</label>
+            <input class="grow" name="Email" id="email" type="email" required />
+        </div>
+
+        <div class="field-wrapper flex-column">
+            <label>País*</label>
+            <select class="grow" name="Pais" id="pais" type="text" required></select>
+        </div>
+
+        <div class="field-wrapper flex-column">
+            <label>Ciudad*</label>
+            <select class="grow" name="Ciudad" id="ciudad" required></select>
+        </div>
+
+        <div class="field-wrapper flex-column">
+            <label>Porfolio link*</label>
+            <input class="grow" name="Porfolio" id="porfolio" type="text" required />
+        </div>
+
+        <div class="field-wrapper flex-column">
+            <label id="file-label">Adjuntar Portfolio</label>
+            <input type="file" name="PorfolioArchivo" id="portfolio-archivo" accept=".mp3,.jpeg,.jpg,.png" />
+            <div class="file-data">MAX <b>5MB</b></div>
+        </div>
+
+        <div class="field-wrapper">
+            <label>Contanos más<br/>
+            de vos y tu proyecto</label>
+            <textarea  name="Descripcion" id="descripcion" placeholder="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit"></textarea>
+        </div>
+
+        
     </div>
+
     <div class="form-step">
-        <div class="form-step-title">Paso 2 Completa el formulario</div>
-        
-        <div class="field-wrapper">
-            <input name="Usuario" id="usuario" type="text" placeholder="Tu usuario / Perfil Domestika" required />
-        </div>
-
-        <div class="field-wrapper">
-            <input name="Nombre" id="nombre" type="text" placeholder="Tu Nombre / Apellido" required />
-        </div>
-
-        <div class="field-wrapper">
-            <input name="Edad" id="edad" type="text" placeholder="Edad" required />
-        </div>
-
-        <div class="field-wrapper">
-            <select name="Pais" id="pais" type="text" required></select>
-        </div>
-
-        <div class="field-wrapper">
-            <select name="Ciudad" id="ciudad" required></select>
-        </div>
-
-        
+    <p class="big-title-2">
+            PASO 3  
+        </p>
+        <p class="big-title-2">
+        Confirmá la/s categorías<br/>
+en las que querés inscribirte
+        </p>
     </div>
 
 
     <div class="form-step">
-        <div class="form-step-title">Paso 3 Elegi la/s categoría/s</div>
         
-        <div class="field-wrapper">
+        
+        <div class="field-wrapper step-categories">
             
             <div class="field-option">
                 <input type="checkbox" name="category" id="Conferencia" value="Conferencia">
-                <label for="conferencia">Conferencia</label>
+                <label for="Conferencia">Conferencia</label>
             </div>
 
             <div class="field-option">
@@ -94,16 +149,38 @@ $countriesAndCities = file_get_contents('countries.min.json');
 
     </div>
 
-    <div class="action-wrapper">
-            <button type="submit">ENVIAR</buton>
+        
+
+        <div class="form-step center">
+            <p class="big-title-2">
+                ¿Todo listo?
+            </p>
+        
+            <button type="submit" id="submit">ENVIAR</buton>
         </div>
 
 </form>
 
 <script>
+    const __DEV__ = document.location.hostname === 'localhost'
+    const MAX_FILE_SIZE = 10240*1000
     const country = document.querySelector('#pais')
     const city = document.querySelector('#ciudad')
     const categories = document.querySelectorAll('input[name="category"]')
+    const fileLabel = document.querySelector('#file-label')
+    const fileInput = document.querySelector('#portfolio-archivo')
+    const fileData = document.querySelector('.file-data')
+    const submit = document.querySelector('#submit')
+    let sending = false
+
+    let serverFileName = ''
+    let isLoading = false
+
+    fileLabel.addEventListener("click", function() {
+        fileInput.click()
+    })
+
+    fileInput.addEventListener("change", uploadFile)
 
     country.innerHTML = Object.keys(countries)
         .map(c => `<option value='${c}' ${c === 'Argentina' ? 'selected' : ''}>${c}</option>`)
@@ -124,24 +201,84 @@ $countriesAndCities = file_get_contents('countries.min.json');
 
     const form = document.forms['register-form']
     
+
+    function uploadFile(e) {
+        const formFile = new FormData(form)
+        
+        isLoading = true
+        const file = fileInput.files[0]
+
+        if (file.size > MAX_FILE_SIZE) {
+            fileData.innerHTML = "El archivo es muy pesado..."
+            setTimeout(() => {
+                fileData.innerHTML = "MAX <b>5MB</b>"
+            }, 3000)
+            return
+        }
+
+        fileData.innerHTML = "Cargando..."
+        formFile.append("file", file);
+
+        fetch('/upload.php', {
+            method: "POST", 
+            body: formFile
+        })
+            .then(res => res.json())
+            .then(res => {
+                if (res.status === 'status_error') {
+                    fileData.innerHTML = "<span style='color:red;'>UPS! ocurrió un error, intenta nuevamente</span>"
+                    setTimeout(() => {
+                        fileData.innerHTML = "MAX <b>5MB</b>"
+                    }, 3000)
+                    return;
+                }
+                fileData.innerHTML = file.name
+                serverFileName = res.file
+                
+            })
+            .catch(err => {
+                fileData.innerHTML = "<span style='color:red;'>UPS! ocurrió un error, intenta nuevamente</span>"
+                setTimeout(() => {
+                    fileData.innerHTML = "MAX <b>5MB</b>"
+                }, 3000)
+            })
+    }
+
+    
     form.addEventListener('submit', e => {
         e.preventDefault()
+        
+        if (sending) {
+            return
+        }
+
+        sending = true
         const formData = new FormData(form)
+        formData.append('Archivo', `${document.location.origin}/uploads/${serverFileName}`)
+        formData.append('PruebaDev', __DEV__)
 
         categories.forEach(category => {
             if (category.checked) {
                 formData.append(category.getAttribute('value'), 'x')
             }
         })
-        
-        fetch(API_SAVE, { method: 'POST', body: formData })
+        submit.innerHTML = '...'
+        fetch(API_SAVE, { 
+            method: 'POST', body: formData
+        })
             .then((res) => {
-                console.log(res.status);
+                sending = false
+                submit.innerHTML = 'ENVIAR'
                 form.reset()
                 return res.text();
             })
-            .then((res) => console.log(res))
-            .catch(error => console.error('Error!', error.message))
+            .then((res) => {
+                sending = false
+            })
+            .catch(error => {
+                submit.innerHTML = 'ENVIAR'
+                sending = false
+            })
     })
 
 </script>
