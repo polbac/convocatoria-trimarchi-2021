883e1afd13caeed3ffb56b75f9f228b9bf701959
juanpinkus <pinkusjuan@gmail.com>
2021-05-27 18:08:41 -0300

    hola falopa

--- index.php
+++ index.php
@@ -1,15 +1,345 @@
 <html>
-    <head>
-        <title>TRIMARCHI</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="assets/style.css">
-        <link rel="stylesheet" href="assets/form.css">
-    </head>
-    <body>
+<head>
+<title>TRIMARCHI</title>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;301;302;303;304;305;306;307;308;309;310;311;312;313;314;315;316;317;318;319;320;321;322;323;324;325;326;327;328;329;330;331;332;333;334;335;336;337;338;339;340;341;342;343;344;345;346;347;348;349;350;351;352;353;354;355;356;357;358;359;360;361;362;363;364;365;366;367;368;369;370;371;372;373;374;375;376;377;378;379;380;381;382;383;384;385;386;387;388;389;390;391;392;393;394;395;396;397;398;399;400;401;402;403;404;405;406;407;408;409;410;411;412;413;414;415;416;417;418;419;420;421;422;423;424;425;426;427;428;429;430;431;432;433;434;435;436;437;438;439;440;441;442;443;444;445;446;447;448;449;450;451;452;453;454;455;456;457;458;459;460;461;462;463;464;465;466;467;468;469;470;471;472;473;474;475;476;477;478;479;480;481;482;483;484;485;486;487;488;489;490;491;492;493;494;495;496;497;498;499;500;501;502;503;504;505;506;507;508;509;510;511;512;513;514;515;516;517;518;519;520;521;522;523;524;525;526;527;528;529;530;531;532;533;534;535;536;537;538;539;540;541;542;543;544;545;546;547;548;549;550;551;552;553;554;555;556;557;558;559;560;561;562;563;564;565;566;567;568;569;570;571;572;573;574;575;576;577;578;579;580;581;582;583;584;585;586;587;588;589;590;591;592;593;594;595;596;597;598;599;600;601;602;603;604;605;606;607;608;609;610;611;612;613;614;615;616;617;618;619;620;621;622;623;624;625;626;627;628;629;630;631;632;633;634;635;636;637;638;639;640;641;642;643;644;645;646;647;648;649;650;651;652;653;654;655;656;657;658;659;660;661;662;663;664;665;666;667;668;669;670;671;672;673;674;675;676;677;678;679;680;681;682;683;684;685;686;687;688;689;690;691;692;693;694;695;696;697;698;699;700;701;702;703;704;705;706;707;708;709;710;711;712;713;714;715;716;717;718;719;720;721;722;723;724;725;726;727;728;729;730;731;732;733;734;735;736;737;738;739;740;741;742;743;744;745;746;747;748;749;750;751;752;753;754;755;756;757;758;759;760;761;762;763;764;765;766;767;768;769;770;771;772;773;774;775;776;777;778;779;780;781;782;783;784;785;786;787;788;789;790;791;792;793;794;795;796;797;798;799;800;801;802;803;804;805;806;807;808;809;810;811;812;813;814;815;816;817;818;819;820;821;822;823;824;825;826;827;828;829;830;831;832;833;834;835;836;837;838;839;840&display=swap" rel="stylesheet">
+<!--link href="https://fonts.googleapis.com/css2?family=Rubik:wght@840;&display=swap" rel="stylesheet"-->
+
+<link rel="stylesheet" href="assets/style.css">
+<link rel="stylesheet" href="assets/form.css">
 
 
-    <?php require('modules/form.php'); ?>
-    </body>
+
+
+<style>
+
+    body {
+        margin: 0;
+        padding: 1.5vw 3vw;
+        font-family: 'Rubik', sans-serif;
+    }
+    
+    .menubar {
+        position: absolute;
+        width: 93.9vw;
+        /*
+        height: 4vw;
+        position: fixed;
+        top: 0;
+        padding-top:1.4vw;
+        border: 1px solid red;
+        */
+    }
+    
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0;
+    }
+
+    header img {
+        
+    }
+
+    header img.domestika {
+        height: 1.6vw;
+    }
+
+    header img.tmdg {
+        height: 1.2vw;
+    }
+
+    header img.tmdg {
+        height: 1.2vw;
+        padding: 0;
+    }
+
+    nav.menu a {
+        font-size: 1.1vw;
+        color: black;
+        margin: 0 .5vw;
+        text-decoration: none;
+    }
+
+    h1 {
+        display: block;
+        font-size: auto;
+        margin-block-start: 0;
+        margin-block-end: 0;
+        margin-inline-start: 0;
+        margin-inline-end: 0;
+        font-weight: inherit;
+    }
+
+    h1.titulo {
+        margin: 1.2vw 0;
+        font-size: 11.6vw;
+        text-align: center;
+        line-height: 0.8;
+        letter-spacing: -1.75vw;
+    }
+
+    h2 {
+        display: block;
+        font-size: auto;
+        margin-block-start: 0;
+        margin-block-end: 0;
+        margin-inline-start: 0;
+        margin-inline-end: 0;
+        font-weight: inherit;
+    }
+
+    h2.subtitulo {
+        margin: 1.2vw 0;
+        font-size: 4.2vw;
+        text-align: center;
+        line-height: 0.8;
+        letter-spacing: -0.6vw;
+    }
+
+    p {
+        display: block;
+        margin-block-start: 0;
+        margin-block-end: 0;
+        margin-inline-start: 0;
+        margin-inline-end: 0;
+    }
+
+    p.destacado {
+        font-size: 2.5vw;
+        text-align: center;
+        line-height: .9;
+        letter-spacing: -0.2vw;
+    }
+
+    p.texto {
+        font-size: 1.2vw;
+        text-align: left;
+        line-height: 1.1;
+        /*letter-spacing: -0.2vw;*/
+    }
+
+    .bloque {
+        margin-top: 5vw;
+        margin-bottom: 5vw;
+        padding-bottom: 7.5vw;
+        border-bottom: 2px solid black;
+    }
+
+    .bloque.uno {
+        /*height: 70.5vh;*/
+
+    }
+
+    .bloque.uno img.animacion {
+        position: absolute;
+        width: 20vw;
+        height: auto;
+        right: 5vw;
+        margin-top:-19vw;
+    }
+
+    .columnas {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: flex-start;
+        padding: 0;
+    }
+
+    .columna {
+        width: 30%;
+        margin-top: 5vw;
+    }
+
+    .columna img {
+        width: 75%;
+        margin-left: 12.5%;
+    }
+
+</style>
+</head>
+
+<body>
+    <div class="menubar">
+        <header>
+            <img class="domestika" src="domestika-logo.svg"/>
+            <nav class="menu">
+                <a href="#" class="menu-item">CONVOCATORIA</a>
+                <a href="#" class="menu-item">CATEGORÍAS+ESTÍMULOS</a>
+                <a href="#" class="menu-item">JURADO</a>
+                <a href="#" class="menu-item">QUIENES SOMOS</a>
+                <a href="#" class="menu-item">F.A.Q</a>
+            </nav>
+            <img class="tmdg"src="tmdg-logo.svg"/>
+        </header>
+    </div>
+
+    <div class="bloque uno">
+        <p class="destacado">
+            Junto a DOMESTIKA lanzamos la
+        </p>
+        <h1 class="titulo">
+            CONVOCATORIA<br />TRIMARCHI 20211<br />JUN-15AGO
+        </h1>
+        <p class="destacado">
+        Llamado abierto a ilustradores, animadores,<br />
+        diseñadores, artistas, emprendedores, educadores,<br />
+        músicos, realizadores audiovisuales y mucho más,<br />
+        para ser parte de diversos proyectos remotos.
+        </p>
+        <img class="animacion" src="reloj.svg"/>
+    </div>
+
+    <div class="bloque dos">
+        <h1 class="titulo">
+            CATEGORIAS
+        </h1>
+        <div class="columnas">
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        CONFERENCIA
+                    </h2>
+                </div>
+                <p class="texto">
+                    Las conferencias son parte del corazón de TRImarchi.
+                    En nuestro camino, hemos gozado con la presencia en nuestros
+                    escenarios de enormes figuras como Seymour Chwast, Paula Scher,
+                    Stefan Sagmeister, Ellen Lupton, entre muchos otros.<br />
+                    Este año, la oportunidad es tuya. Si querés hablarle a la
+                    comunidad sobre tu carrera, proyectos y experiencias,
+                    inscribite en esta categoría y participa por:
+                </p>
+            </div>
+
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        EXPO
+                    </h2>
+                </div>
+                <p class="texto">
+                    Artistas muy diversos siempre han tenido lugar en TRImarchi
+                    para mostrar su trabajo. Sabemos que hay muchas buenas ideas
+                    que solo necesitan una mano amiga para ver la luz. Por eso,
+                    si tenés en mente una obra artística para desarrollar y exponer,
+                    inscribite en esta categoría y participa por:
+                </p>
+            </div>
+
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        WORKSHOP
+                    </h2>
+                </div>
+                <p class="texto">
+                    Una de las razones por las que nace TRImarchi es saciar
+                    la sed de quienes buscan data. Si tenés vocación de enseñar
+                    y querés compartir lo que sabés, anotate en esta categoría
+                    para participar por la posibilidad de:
+                </p>
+            </div>
+
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        INDUMENTARIA
+                    </h2>
+                </div>
+                <p class="texto">
+                    ¿Querés crear o darle impulso a tu marca de indumentaria?
+                    Éste es tu lugar dentro de la convocatoria. Inscribite en
+                    esta categoría para:
+                </p>
+            </div>
+
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        EMPRENDIMIENTO
+                    </h2>
+                </div>
+                <p class="texto">
+                    Si tenés un proyecto personal vinculado a la creatividad
+                    o el arte al que querés hacer crecer, este es tu lugar
+                    dentro de la convocatoria. Anotate acá para:
+                </p>
+            </div>
+
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        MÚSICA
+                    </h2>
+                </div>
+                <p class="texto">
+                    Si sos una banda o músico solista, no podés perderte
+                    esta oportunidad. Compartinos una canción de tu autoría
+                    y si sos elegido:
+                </p>
+            </div>
+
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        VIDEOCLIP
+                    </h2>
+                </div>
+                <p class="texto">
+                    En esta categoría buscamos personas que quieran dirigir
+                    un videoclip. Inscribite y participá por:
+                </p>
+            </div>
+
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        PORTADA
+                    </h2>
+                </div>
+                <p class="texto">
+                    En esta categoría unimos muchas cosas que amamos:
+                    diseño, ilustración, tipografía, música, etc. Inscribite para:
+                </p>
+            </div>
+
+            <div class="columna">
+                <div class="check-categoria">
+                    <img class="animacion" src="reloj.svg"/>
+                    <h2 class="subtitulo">
+                        COLECTIVO
+                    </h2>
+                </div>
+                <p class="texto">
+                    Inscribite en esta categoría para tener la posibilidad de trabajar
+                    junto a nosotros en proyectos puntuales que tendrán lugar a lo
+                    largo del año. Hay muchas ideas dando vueltas, así que la búsqueda
+                    es amplia: ilustradorxs, diseñadorxs, tipógrafxs, fotógrafxs,
+                    ceramistas, artesanos y un largo etcétera. No hay plazo límite para
+                    inscribirse si querés formar parte del colectivo TRImarchi, pero no
+                    duermas, porque hay proyectos que comienzan muy pronto.
+                </p>
+            </div>
+        
+        </div>
+    </div>
+
+    <div class="bloque tres">
+        <?php require('modules/form.php'); ?>
+    </div>
+
+</body>
 
 </html>
